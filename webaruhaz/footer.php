c5e5af5d31999cf32d3672bcc9e3a524b12f7a06
kuruczmilan@gmail.com <Tanulo@M4-03>
2021-11-16 10:34:00 +0100

    Backup

---
+++ webaruhaz/footer.php
@@ -0,0 +1,9 @@
+<?php
+echo '<div class="jumbotron text-center" style="margin-bottom:0">
+<p>
+    Készítette: Kurucz Milán
+    <br>
+    2021
+</p>
+</div>';
+?>
---
+++ webaruhaz/kapcsolat.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "localhost";
+$username = "webaruhaz_km";
+$password = "Kwjsu39a";
+$dbname = "webaruhaz_km";
+?>
